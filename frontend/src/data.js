b7cc9d4e99b954580a72891754c38ec2efb8d1b5
kollie-coder <apotiolasegun@gmail.com>
2021-11-03 15:24:11 +0000

    Benji init

---
+++ frontend/src/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+    {
+        _id:'1',
+         name:'DELL INSPIRON 3000 SERIES',
+         category:'LAPTOP',
+        image:'/images/p1.jpg',
+        price: '450000',
+        brand: 'DELL',
+        rating: '4.5',
+        numReviews: 10,
+        description: 'high quality product',
+
+       },
+       {
+        _id:'2',
+         name:'HP LAPTOP',
+         category:'LAPTOP',
+        image:'/images/p2.jpg',
+        price: '350000',
+        brand: 'HP',
+        rating: '4.0',
+        numReviews: 10,
+        description: 'high quality product',
+
+       },
+       {
+        _id:'3',
+         name:'HP LAPTOP',
+         category:'LAPTOP',
+        image:'/images/p3.jpg',
+        price: '250000',
+        brand: 'HP',
+        rating: '4.8',
+        numReviews: 10,
+        description: 'high quality product',
+
+       },
+       {
+        _id:'4',
+         name:'SAMSUNG S20',
+         category:'PHONE',
+        image:'/images/p4.jpg',
+        price: '500000',
+        brand: 'SAMSUNG',
+        rating: '4.5',
+        numReviews: 17,
+        description: 'high quality product',
+
+       },
+       {
+        _id:'5',
+         name:'ACER LAPTOP',
+         category:'LAPTOP',
+        image:'/images/p5.jpg',
+        price: '250000',
+        brand: 'ACER',
+        rating: '4.5',
+        numReviews: 10,
+        description: 'high quality product',
+
+       },
+       {
+        _id:'6',
+         name:'APPLE MACBOOK PRO',
+         category:'LAPTOP',
+        image:'/images/p6.jpg',
+        price: '800000',
+        brand: 'APPLE',
+        rating: '4.5',
+        numReviews: 10,
+        description: 'high quality product',
+
+       },
+
+    ],
+};
+export default data;
